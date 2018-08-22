0effc54dc89e67cb58fcc9a466a9f322353c7b9a
Pablo De La Cruz <squall07524@gmail.com>
2018-08-22 13:58:33 -0400

    restructuring step1

--- main.js
+++ main.js
@@ -1,15 +1,79 @@
-// console.log(data);
-// const Promise = require("axios");
-// axios.get("http://swapi.co/api/planets/1/")
-axios.get("http://fizal.me/pokeapi/api/1.json")
-.then(result =>{
-    console.log(result.data);
-    
-})
-.catch(err => {
-    console.log("Err!!!", err);
-    
-});
+function createPokemon(id) {
+    let pokemon;
+    //random background
+    let backgroundArr = [
+        "https://www.fg-a.com/wallpapers/white-marble-1-2018.jpg",
+        "https://img.freepik.com/free-vector/bright-background-with-dots_1055-3132.jpg?size=338&ext=jpg",
+        "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_960_720.jpg",
+        "https://png.pngtree.com/thumb_back/fw800/back_pic/03/70/72/5257b6c12d89875.jpg",
+        "https://www.planwallpaper.com/static/images/violet-vector-leaves-circles-backgrounds-for-powerpoint_PdfkI4q.jpg"
+    ];
+
+     axios.get(`http://fizal.me/pokeapi/api/${id}.json`)
+        .then(result => {
+            let res = result.data;
+
+            //get abilities
+            let abilitiesArr = [];
+            res.abilities.forEach(element => {
+                abilitiesArr.push(element.ability.name);
+            });
+
+            pokemon = new Pokemon(res.name, res.stats[5].base_stat, res.stats[4].base_stat, res.stats[3].base_stat, abilitiesArr);
+
+            let container = document.getElementById("container");
+            container.innerHTML += `
+                                        <div class="col s12 m4 ">
+                                        <div class="card" >
+                                            <div class="card-image cardImage" style="background-image: url(${backgroundArr[Math.floor(Math.random() * Math.floor(backgroundArr.length))]})">
+                                            <img src= "https://assets.pokemon.com/assets/cms2/img/pokedex/full/${padId(id)}.png">
+                                            <span class="card-title card-title-style">${pokemon.name}</span>
+                                            </div>
+                                            <div class="card-content">
+                                            <div class="card-panel red">HP: ${pokemon.hp}</div>
+                                            <div class="card-panel red lighten-1">Att: ${pokemon.attack}</div>
+                                            <div class="card-panel red lighten-2">Def: ${pokemon.defense}</div>
+                                            <div class="card-panel red lighten-3">
+                                            <div class="responsive-table table-status-sheet">
+                                            <table class="bordered">
+                                                <thead>
+                                                    <tr>
+                                                        <th class="center">Abilities:</th>
+                                                        <th id="totalRepos" class="center"></th>
+                    
+                                                    </tr>
+                                                </thead>
+                                                <tbody id="tbody${id}">
+                    
+                                                </tbody>
+                                            </table>
+                                            </div>
+                                            <div class="card-action">
+                                            <a href="#">More</a>
+                                            </div>
+                                        </div>
+                                        </div>
+                                    `;
+
+
+            const tbody = document.getElementById(`tbody${id}`);
+
+            pokemon.abilities.forEach(element => {
+                let tr = document.createElement("tr");
+                tr.innerHTML = `<td>
+                                <a href="">
+                                    <div>${element}</div>
+                                </a>
+                            </td>`;
+                tbody.appendChild(tr);
+            });
+            return pokemon;
+        })
+        .catch(err => {
+            console.log("Err!!!", err);
+
+        });
+}
 class Trainer {
     constructor(name, pokemons) {
         this.name = name;
@@ -26,53 +90,37 @@ class Trainer {
 }
 
 class Pokemon {
-    constructor(hp, attack, defense, abilities) {
+    constructor(name, hp, attack, defense, abilities) {
+        this.name = name;
         this.hp = hp;
         this.attack = attack;
         this.defense = defense;
         this.abilities = abilities;
     }
 
+    getName() {
+        return this.name;
+    }
 }
-let pokemon1 = new Pokemon(9999, 9999, 9999, ["headbutt", "omnislash"]);
-let pokemon2 = new Pokemon(5, 23, 12, ["run", "hide"]);
-let pokemon3 = new Pokemon(1, 1, 9999, ["headbutt", "omnislash"]);
 
-let trainer = new Trainer("Pablo", [pokemon1, pokemon2, pokemon3]);
-// console.log("pokemon", pokemon1);
+//Helper Functions
+let padId = id => {
+    let s = id + "";
+    while (s.length < 3) {
+        s = "0" + s;
+    }
+    return s;
+};
 
-// console.log("trainer",trainer);
+let trainer = new Trainer("Pablo", [createPokemon(6), createPokemon(39), createPokemon(151)]);
+console.log("trainer", trainer);
 
-// const getPost = () => {
-// return fetch(`https://jsonplaceholder.typicode.com/todos/1`)
-// .then(res => res.json())
-// .then(posts => console.log(posts))
-// .catch(function(e) {
-//     console.log(e); });// "oh, no!"
-// };
+document.addEventListener('DOMContentLoaded', function () {
+    let elems = document.querySelectorAll('.carousel');
+    let instances = M.Carousel.init(elems);
+});
 
-// // let pokedata = getPost();
-// // console.log("pokedata", pokedata);
-
-
-// function onClickTest() {
-//     window.resizeTo("1200", "800");
-//     console.log("hello");
-
-// }
-// document.addEventListener('DOMContentLoaded', function () {
-//     var elems = document.querySelectorAll('.modal');
-//     var instances = M.Modal.init(elems);
-//     console.log(instances);
-
-// });
- 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems);
-  });
-
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.materialboxed');
-    var instances = M.Materialbox.init(elems);
-  });
+document.addEventListener('DOMContentLoaded', function () {
+    let elems = document.querySelectorAll('.materialboxed');
+    let instances = M.Materialbox.init(elems);
+});
